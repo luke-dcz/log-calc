ac8fc4f3f81f1f30cfd767096d23723ef137506e
luke-dcz <luke.dacruz.2020@live.rhul.ac.uk>
2023-09-20 14:21:32 +0100

    base project outline

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,16 @@
 import './globals.css';
 import { Suspense } from 'react';
-import { Inter } from 'next/font/google'; //update import to chosen font
-import Link from 'next/link';
+import { Inter } from 'next/font/google';
 import Loading from './loading';
 import NotFound from './not-found';
 import ErrorBoundary from '../components/error-boundary';
 
-const inter = Inter({ subsets: ['latin'] }); //Change this line to your chosen font
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'YOUR APP NAME', //Change this to the title of your app
-  description: 'Generated with create next app with custom template', //Add a small description of your app here
+  title: 'Logarithm Calculator',
 };
 
-//Change this layout to whatever base layout for each pages
 export default function RootLayout({
   children,
 }: {
@@ -22,19 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        {/* import your navbar/footer layout component here (replace the div too) */}
-        <div className="flex gap-x-2">
-          <Link href={'/'} className="">
-            Home
-          </Link>
-          <Link href={'/other-page'}>Other page</Link>
-        </div>
         <Suspense fallback={<Loading />}>
-          <ErrorBoundary fallback={<NotFound />}>
-            <div className={inter.className}>{children}</div>
-          </ErrorBoundary>
+          <ErrorBoundary fallback={<NotFound />}>{children}</ErrorBoundary>
         </Suspense>
-        {/* import your footer layout component here */}
       </body>
     </html>
   );
